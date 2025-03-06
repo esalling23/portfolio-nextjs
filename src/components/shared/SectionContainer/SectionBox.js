a90a8676ec13f3579c8ec04dc44d4fb53390c4b8
Eron Salling <esalling23@gmail.com>
2025-03-06 16:54:44 -0500

    Adds onEnter handler for section boxes Will be run after box sections complete their initial fade in

--- src/components/shared/SectionContainer/SectionBox.js
+++ src/components/shared/SectionContainer/SectionBox.js
@@ -9,6 +9,7 @@ import styles from './styles.module.scss';
 const SectionBox = ({
 	children,
 	className,
+  onEnter = () => {},
 	...rest
 }) => {
 	return <section 
@@ -21,7 +22,8 @@ const SectionBox = ({
 	>
 		<motion.div
 			initial={{ opacity: 0, }}
-			whileInView={{ opacity: 1, transition: { duration: 1, ease: 'easeInOut' } }}
+      onAnimationComplete={onEnter}
+			whileInView={{ opacity: 1, transition: { duration: 0.5, ease: 'easeInOut' } }}
 			viewport={{ once: true, amount: .25 }}
 			className={classNames(styles.content, 'flex flex-col')}
 		>
